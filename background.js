57a14fa7a32fce8ba5c6d70e5f70eafafc07b0e6
Cui Zhen | Saism | TRVDD <songsongdahu233@gmail.com>
2018-06-13 14:26:52 +0900

    Make it execute every time the url changes on github

--- background.js
+++ background.js
@@ -5,15 +5,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'github.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+  chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    chrome.tabs.executeScript(null, { file: "src/index.js" });
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-console.log("This extension works!");
-
 function getTitleTree(readme) {
   let article = readme.querySelectorAll('article')[0];
   let childs = Array.from(article.childNodes);
@@ -12,61 +10,67 @@ function getTitleTree(readme) {
   return result;
 }
 
-// styles
-let newStyle = document.createElement("style"); 
-newStyle.innerHTML = `
-  .navi {
-    width: 350px;
-    position: fixed;
-    top: 300px;
-    left: 101px;
-  }
+function start() {
+  console.log("This extension started!");
 
-  .navi-content {
-    max-height: 450px;
-    overflow: auto;
-  }
+  // styles
+  let newStyle = document.createElement("style"); 
+  newStyle.innerHTML = `
+    .navi {
+      width: 350px;
+      position: fixed;
+      top: 300px;
+      left: 101px;
+    }
 
-  .navi-content .level_h1 {
-    margin-left: 10px;
-  }
+    .navi-content {
+      max-height: 450px;
+      overflow: auto;
+    }
 
-  .navi-content .level_h2 {
-    margin-left: 25px;
-    list-style-type: circle;
-  }
+    .navi-content .level_h1 {
+      margin-left: 10px;
+    }
 
-  .navi-content .level_h3 {
-    margin-left: 40px;
-  }
+    .navi-content .level_h2 {
+      margin-left: 25px;
+      list-style-type: circle;
+    }
 
-  .navi-content .level_h4 {
-    margin-left: 55px;
-    list-style-type: circle;
-  }
+    .navi-content .level_h3 {
+      margin-left: 40px;
+    }
 
-  .navi-content .level_h5 {
-    margin-left: 60px;
-  }
-`;
-document.querySelector('head').appendChild(newStyle)
+    .navi-content .level_h4 {
+      margin-left: 55px;
+      list-style-type: circle;
+    }
 
-// main
-const readme = document.querySelector('#readme');
-const lists = getTitleTree(readme);
-const newDiv = document.createElement("div"); 
-newDiv.innerHTML = `
-<div class="navi Box Box--condensed mb-3 js-repos-container" data-pjax-container role="navigation">
-  <div class="Box-header">
-    <h3 class="Box-title d-flex flex-justify-between flex-items-center">
-      Navigation
-    </h3>
+    .navi-content .level_h5 {
+      margin-left: 60px;
+    }
+  `;
+  document.querySelector('head').appendChild(newStyle);
+
+  // main
+  const readme = document.querySelector('#readme');
+  const lists = getTitleTree(readme);
+  const newDiv = document.createElement("div"); 
+  newDiv.innerHTML = `
+  <div class="navi Box Box--condensed mb-3 js-repos-container" data-pjax-container role="navigation">
+    <div class="Box-header">
+      <h3 class="Box-title d-flex flex-justify-between flex-items-center">
+        Navigation
+      </h3>
+    </div>
+    <div class="Box-body navi-content">
+      <ul>
+        ${lists.join('\n')}
+      </ul>
+    </div>
   </div>
-  <div class="Box-body navi-content">
-    <ul>
-      ${lists.join('\n')}
-    </ul>
-  </div>
-</div>
-`;
-readme.appendChild(newDiv);
+  `;
+  readme.appendChild(newDiv);
+}
+
+start();
