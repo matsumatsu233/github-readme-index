91b094ac25bc22e457198c297e732e6617c2644a
Cui Zhen | Saism | TRVDD <songsongdahu233@gmail.com>
2018-06-19 10:22:50 +0900

    Make Navigation sticky

--- src/navi.js
+++ src/navi.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Radium from 'radium';
 
 const styles = {
-  navi: {
+  naviAbsolute: {
+    width: 350,
+    position: 'absolute',
+    top: 0,
+    left: -370,
+    zIndex: 1000,
+  },
+  naviFixed: {
     width: 350,
     position: 'fixed',
-    top: 300,
-    left: 101,
+    top: 0,
+    left: -370,
     zIndex: 1000,
   },
   naviContent: {
@@ -32,21 +39,72 @@ const styles = {
   },
 };
 
-function Navi(props) {
-  return (
-    <div className="Box Box--condensed mb-3 js-repos-container" style={styles.navi} data-pjax-container role="navigation">
-      <div className="Box-header">
-        <h3 className="Box-title d-flex flex-justify-between flex-items-center">
-          Navigation
-        </h3>
+const naviContainerStyles = ({isFixed, readmeContainerX}) => {
+  if (isFixed) {
+    return {
+      width: 350,
+      position: 'fixed',
+      top: 30,
+      left: readmeContainerX - 370 + 1,
+      zIndex: 1000,
+    };
+  } else {
+    return {
+      width: 350,
+      position: 'absolute',
+      top: 0,
+      left: -370,
+      zIndex: 1000
+    }
+  }
+
+}
+
+class Navi extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scrollY: 0
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    this.setState({
+      scrollY: window.scrollY
+    });
+  }
+
+  render() {
+    const readmeContainerX = document.querySelector('.file').getBoundingClientRect().x;
+    const readmeContainerY = document.querySelector('.file').getBoundingClientRect().y;
+    const boundary = readmeContainerY + this.state.scrollY;
+
+    const specialStyle = this.state.scrollY < (boundary - 30) ? styles.naviAbsolute : styles.naviFixed;
+
+    return (
+      <div className="Box Box--condensed mb-3 js-repos-container" style={naviContainerStyles({isFixed: this.state.scrollY > (boundary - 30), readmeContainerX})} data-pjax-container role="navigation">
+        <div className="Box-header">
+          <h3 className="Box-title d-flex flex-justify-between flex-items-center">
+            Navigation
+          </h3>
+        </div>
+        <div className="Box-body" style={styles.naviContent}>
+          <ul>
+            {this.props.items.map((item) => <li style={styles[`level_${item.level}`]}><a href={item.hash}>{item.text}</a></li>)}
+          </ul>
+        </div>
       </div>
-      <div className="Box-body" style={styles.naviContent}>
-        <ul>
-          {props.items.map((item) => <li style={styles[`level_${item.level}`]}><a href={item.hash}>{item.text}</a></li>)}
-        </ul>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Radium(Navi);
